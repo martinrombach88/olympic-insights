267dcf68822cdc2d48d9adacc96e8b2998c78a09
Martin Rombach <martinrombach88@gmail.com>
2024-02-11 23:10:46 +0000

    install packages

---
+++ Data/DataContext.cs
@@ -0,0 +1,8 @@
+// namespace Olympics.Data
+// {
+
+//     public class DataContext : DbContext
+//     {
+
+//     }
+// }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Olympics 
+{
+    internal class Program
+    {
+        // do you need this if you're 'in main'? you've written the old template in the new template
+        //also you used the console app template so be prepared to remove this later
+        static void Main(string[] args) {
+
+        }
+    }
+}
