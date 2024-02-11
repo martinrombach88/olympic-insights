4ecccfd1417fa2845fc874d472663ae043a7f5bd
Martin Rombach <martinrombach88@gmail.com>
2024-02-11 21:15:57 +0000

    scrum notes

--- obj/Debug/net8.0/olympic-insights.AssemblyInfo.cs
+++ obj/Debug/net8.0/olympic-insights.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("olympic-insights")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f782b6a9faa643a72923f2a40f36794c434f18c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("olympic-insights")]
 [assembly: System.Reflection.AssemblyTitleAttribute("olympic-insights")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
