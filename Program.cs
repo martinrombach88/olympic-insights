f2d569fff18751e25fff355e509810c402f1fc76
Martin Rombach <martinrombach88@gmail.com>
2024-02-29 06:34:13 +0900

    test build, add packages

--- Program.cs
+++ Program.cs
@@ -7,25 +7,30 @@ namespace OlympicInsights
     {
         static void Main(string[] args) {
 
-        //app updated to web app to make this line work
+        //app updated to web app to make this line work (see csproj sdk, consider web app template)
         var builder = WebApplication.CreateBuilder(args);
+        //class of web application in docs: https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.builder.webapplication?view=aspnetcore-8.0
 
-        //do we have controllers yet?
+        //methods of the services property of the web application class
+        //https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.builder.webapplicationbuilder.services?view=aspnetcore-8.0#microsoft-aspnetcore-builder-webapplicationbuilder-services
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
 
-        //this line doesn't work because?
-        //builder.Services.AddSwaggerGen();
-            Athlete steve = new Athlete()
-            {
-                name="Steve",
-                gender="male",
-                team="UK",
-                height=183,
-                weight=200,
-                sportEvents=["tight rope", "cheese rolling"],
-                medals=["gold", "silver"],
-            };
+        //install swashbuckle to enable this
+        builder.Services.AddSwaggerGen();
+
+        //builder.Services.AddSqlite<OlympicContext>("Data Source=ContosoPizza.db");
+        
+            // Athlete steve = new Athlete()
+            // {
+            //     name="Steve",
+            //     gender="male",
+            //     team="UK",
+            //     height=183,
+            //     weight=200,
+            //     sportEvents=["tight rope", "cheese rolling"],
+            //     medals=["gold", "silver"],
+            // };
 
         }
     }
--- obj/Debug/net8.0/olympic-insights.AssemblyInfo.cs
+++ obj/Debug/net8.0/olympic-insights.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("olympic-insights")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037650a26ec4a5851eae695df719adf29aded3ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2928cc99d12891a0135f4c0100e7a7443631245")]
 [assembly: System.Reflection.AssemblyProductAttribute("olympic-insights")]
 [assembly: System.Reflection.AssemblyTitleAttribute("olympic-insights")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
